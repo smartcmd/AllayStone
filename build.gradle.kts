ed35ce6582e4c12f001e437ee261d9f12575d21e
daoge_cmd <3523206925@qq.com>
2026-03-29 17:10:23 +0800

    The next version is 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "org.allaymc.allaystone"
 description = "A python plugin loader & runtime for AllayMC using GraalPython, inspired by Endstone"
-version = "0.1.0"
+version = "0.1.1-SNAPSHOT"
 val allayApiVersion = "0.27.0"
 
 java {
