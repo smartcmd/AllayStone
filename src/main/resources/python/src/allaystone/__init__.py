32192530294f5fde0b968e0005a0e7b1707ce7cb
daoge_cmd <3523206925@qq.com>
2026-03-29 15:21:12 +0800

    docs: annotate Python plugin helper package

--- src/main/resources/python/src/allaystone/__init__.py
+++ src/main/resources/python/src/allaystone/__init__.py
@@ -1,3 +1,5 @@
+"""Python-side helpers exposed by AllayStone to plugin authors."""
+
 from .plugin import Plugin
 
 __all__ = ["Plugin"]
--- src/main/resources/python/src/allaystone/plugin.py
+++ src/main/resources/python/src/allaystone/plugin.py
@@ -1,4 +1,7 @@
 class Plugin:
+    """Base class for Python plugins loaded by AllayStone."""
+
+    # Descriptor fields read by the Java-side plugin loader.
     version = None
     api_version = None
     description = None
@@ -7,6 +10,7 @@ class Plugin:
     depend = None
     soft_depend = None
 
+    # Runtime objects injected after the plugin instance is created.
     server = None
     logger = None
     data_folder = None
@@ -14,10 +18,13 @@ class Plugin:
     java_plugin = None
 
     def on_load(self):
+        """Called before worlds are loaded."""
         pass
 
     def on_enable(self):
+        """Called after worlds are loaded and the plugin is being enabled."""
         pass
 
     def on_disable(self):
+        """Called while the server is shutting down and the plugin is being disabled."""
         pass
