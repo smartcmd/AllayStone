97bcb256679a089ecc3f984e5d875bc45a53d863
daoge_cmd <3523206925@qq.com>
2026-03-30 20:30:14 +0800

    Tighten Python plugin runtime stub types

--- src/main/resources/python/src/allaystone/plugin.pyi
+++ src/main/resources/python/src/allaystone/plugin.pyi
@@ -1,5 +1,7 @@
 from typing import Any, ClassVar, Optional, Sequence
 
+from allay.api.plugin.Plugin import Plugin as JavaPlugin
+from allay.api.server.Server import Server
 
 class Plugin:
     version: ClassVar[Optional[str]]
@@ -10,11 +12,11 @@ class Plugin:
     depend: ClassVar[Optional[Sequence[str]]]
     soft_depend: ClassVar[Optional[Sequence[str]]]
 
-    server: Any
+    server: Server
     logger: Any
     data_folder: str
     name: str
-    java_plugin: Any
+    java_plugin: JavaPlugin
 
     def on_load(self) -> None: ...
     def on_enable(self) -> None: ...
